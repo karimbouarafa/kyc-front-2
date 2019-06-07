c917b7af4dc98c5582ebe8048d395fcddcde12f3
PierreOlive <pierre.olive4@gmail.com>
2019-06-07 15:36:04 +0200

    services code refactoring

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -4,31 +4,22 @@ import { Data } from '../../../node_modules/@angular/router';
 
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ClientService {
-  
-  
-  nom: string;
-  prénom: string;
-  datedenaissance: string;
-  
-  
+	nom: string;
+	prénom: string;
+	datedenaissance: string;
+	constructor(private httpClient: HttpClient) {
+	}
 
-  constructor(private httpClient: HttpClient ) {
-                
-   
-    
-   }
-
-   idclient(){
-    console.log("ooo")
-    var str = new String(this.nom.concat(this.prénom).concat(this.datedenaissance)); 
-    console.log(str);
-    console.log(str.toString());
-
-       return str.toString();
-   }
+	idclient() {
+		console.log("ooo")
+		var str = new String(this.nom.concat(this.prénom).concat(this.datedenaissance));
+		console.log(str);
+		console.log(str.toString());
+		return str.toString();
+	}
 
 
 
--- src/app/services/document.service.ts
+++ src/app/services/document.service.ts
@@ -5,140 +5,131 @@ import { Docu } from '../models/document.model';
 import { ClientService } from './client.service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DocumentsService {
-  
 
-  constructor(private httpClient: HttpClient, private clientService: ClientService) { 
-    
-      this.getDocumentsFromServer();
-      
-    
-    } 
-  
-  documents: Docu[] = [];
-  mydocs: Docu[] = [];
-  notmydocs: Docu[] = [];
-  documentsSubject = new Subject<Docu[]>();
-     
-  emitdocuments() {
-    this.documentsSubject.next(this.documents);
-this.MyDocuments();
-    this.notMyDocuments();
-  }
-  
-  clientexist(){
-    if(this.documents.length = 0) { return false}
-    else {return true;}
-  }
-  adddocument(d: Docu){
-    this.mydocs.push(d);
-  }
- 
 
-  getDocumentsFromServer() {
+	constructor(private httpClient: HttpClient, private clientService: ClientService) {
 
-    
+		this.getDocumentsFromServer();
 
-        
-    this.httpClient
-      .get<any[]>('http://localhost:10010/api/template/dossier?client='+this.clientService.idclient())
-      .subscribe(
-        (response) => {
-          let res = response;
-         
-         
-         for(var _i = 0; _i < res.length; _i++){
-            
-          this.documents.push(res[_i]['state']['data']);
-          }
-          
-         this.emitdocuments();
-          
-          
-        },
-        (error) => {
-          console.log('Erreur ! : ' + error);
-        }
-      );
+
+	}
+
+	documents: Docu[] = [];
+	mydocs: Docu[] = [];
+	notmydocs: Docu[] = [];
+	documentsSubject = new Subject<Docu[]>();
+
+	emitdocuments() {
+		this.documentsSubject.next(this.documents);
+		this.MyDocuments();
+		this.notMyDocuments();
+	}
+
+	clientexist() {
+		if (this.documents.length = 0) { return false }
+		else { return true; }
+	}
+	adddocument(d: Docu) {
+		this.mydocs.push(d);
+	}
+
+
+	getDocumentsFromServer() {
+		this.httpClient
+			.get<any[]>('http://localhost:10010/api/template/dossier?client=' + this.clientService.idclient())
+			.subscribe(
+				(response) => {
+					let res = response;
+					for (var _i = 0; _i < res.length; _i++) {
+						this.documents.push(res[_i]['state']['data']);
+					}
+					this.emitdocuments();
+				},
+				(error) => {
+					console.log('Erreur ! : ' + error);
+				}
+			);
+	}
+
+
+
+
+	CreateDoc(nomdoc: string, status: string, expire: string) {
+		this.httpClient
+			.put("http://localhost:10010/api/template/CreateDoc?client=" + this.clientService.idclient() + "&doc=0&status=" + status + "&nomdoc=" + nomdoc + "&expire=" + expire, {
+				"status": "33",
+				"expire": "44",
+				"nomdoc": "12",
+			})
+			.subscribe(
+				() => {
+					console.log('document crée réalisé');
+				},
+				(error) => {
+					console.log('Erreurrrr ! : ' + error);
+				}
+			);
+	}
+
+
+
+	MyDocuments() {
+
+		for (var _j = 0; _j < this.documents.length; _j++) {
+			if (this.documents[_j]['initiator'] == "O=Natixis Assurance, L=Paris, C=FR") {
+				this.mydocs.push(this.documents[_j])
+			}
+		}
+
+	}
+	notMyDocuments() {
+
+		for (var _j = 0; _j < this.documents.length; _j++) {
+			if (this.documents[_j]['initiator'] != "O=Natixis Assurance, L=Paris, C=FR") {
+				this.notmydocs.push(this.documents[_j])
+			}
+		}
+	}
+
+	getDocumentInformation(client: string, doc: string) {
+
+		this.httpClient.get<any[]>('http://localhost:10010/api/template/dossier?client=10', {
+			params: {
+				doc: doc,
+				client: client
+			}
+		})
+			.subscribe(
+				(response) => {
+					let res = response;
+					return res['state']['data']
+				},
+				(error) => {
+					console.log('Erreur ! : ' + error);
+				});
+	}
+
+	getListDocumentOfClient(client: string) {
+
+		this.httpClient.get<any[]>('http://localhost:10010/api/template/dossier?client=10', {
+			params: {
+				client: client
+			}
+		})
+			.subscribe(
+				(response) => {
+					let res = response;
+					for (var _i = 0; _i < res.length; _i++) {
+						var X: Document[] = [];
+						X.push(res[_i]['state']['data']);
+					}
+					return X;
+				},
+				(error) => {
+					console.log('Erreur ! : ' + error);
+				});
+	}
 }
-
-
-
-
-CreateDoc(nomdoc: string, status: string, expire: string) {
-    
-  this.httpClient
-  .put("http://localhost:10010/api/template/CreateDoc?client="+this.clientService.idclient()+"&doc=0&status="+status+"&nomdoc="+nomdoc+"&expire="+expire,{
-    "status": "33",
-    "expire": "44",
-    "nomdoc": "12",
-        })
-    .subscribe(
-      () => {
-        console.log('document crée réalisé');
-                },
-      (error) => {
-        console.log('Erreurrrr ! : ' + error);
-      }
-    );
-    }
-
-
-
-    MyDocuments(){
-         
-        for(var _j = 0; _j < this.documents.length; _j++){
-            if(this.documents[_j]['initiator'] == "O=Natixis Assurance, L=Paris, C=FR"){
-                this.mydocs.push(this.documents[_j])
-            }
-        }
-       
-    }
-    notMyDocuments(){
-
-      for(var _j = 0; _j < this.documents.length; _j++){
-        if(this.documents[_j]['initiator'] != "O=Natixis Assurance, L=Paris, C=FR"){
-            this.notmydocs.push(this.documents[_j])
-        }
-    }
-    }
-
-    getDocumentInformation(client: string, doc: string){
-        
-        this.httpClient.get<any[]>('http://localhost:10010/api/template/dossier?client=10',{
-            params: {
-              doc: doc,
-              client: client
-            }})
-      .subscribe(
-        (response) => {
-          let res = response;
-          return res['state']['data']
-        },
-        (error) => {
-          console.log('Erreur ! : ' + error);
-        });
-            }
-     
-    getListDocumentOfClient(client: string){
-        
-                this.httpClient.get<any[]>('http://localhost:10010/api/template/dossier?client=10',{
-                    params: {
-                      client: client
-                    }})
-              .subscribe(
-                (response) => {
-                  let res = response;
-                  for(var _i = 0; _i < res.length; _i++){
-                    var X: Document[] = [];
-                    X.push(res[_i]['state']['data']);
-                    }
-                  return X;
-                },
-                (error) => {
-                  console.log('Erreur ! : ' + error);
-                });
-                    }
-      }
