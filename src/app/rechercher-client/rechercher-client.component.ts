9dd1e379b88c734d6f51a2bc187db0fb0e60fa51
mohammedkarim.bouara <mohammedkarim.bouarafa@bearingpoint.com>
2019-06-21 11:27:30 +0200

    Document integration 2

--- src/app/rechercher-client/rechercher-client.component.ts
+++ src/app/rechercher-client/rechercher-client.component.ts
@@ -26,25 +26,20 @@ export class RechercherClientComponent implements OnInit {
 		this.entiteUtilisateur=this.userCredentialService.getEntiteUser()
 		this.atService.getDocumentEntiteList();
 	}
-
 	onSubmit(form: NgForm) {
 		this.b = true;
 		console.log('recherche client...');
 		this.clientService.nom = form.value['nom'];
 		this.clientService.prénom = form.value['prénom'];
 		this.clientService.datedenaissance = form.value['datedenaissance'];
-
 		if (this.isClientFound()) {
-
 			this.clientService.civilité = this.clientexistService.getClient(form.value['nom'], form.value['prénom'], form.value['datedenaissance'])['civilité'];
 			this.clientService.lieudenaissance = this.clientexistService.getClient(form.value['nom'], form.value['prénom'], form.value['datedenaissance'])['lieudenaissance'];
 			this.clientService.adressepostale = this.clientexistService.getClient(form.value['nom'], form.value['prénom'], form.value['datedenaissance'])['adressepostale'];
 			this.router.navigate(['/dossier-client', this.clientService.idclient(), this.entiteUtilisateurRouting]);
 		}
 	}
-
 	isClientFound() {
-
 		return this.clientexistService.doesClientExist(this.clientService.nom, this.clientService.prénom, this.clientService.datedenaissance);
 	}
 
